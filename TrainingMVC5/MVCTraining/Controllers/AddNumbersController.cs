4e60ff915b7ca93aa305c52f7073d33220e7bc24
Ana Maria Ciuhan <ana.ciuhan@fortech.ro>
2017-05-16 17:58:46 +0300

    Signed-off-by: Ana Maria Ciuhan <ana.ciuhan@fortech.ro>

---
+++ TrainingMVC5/MVCTraining/Controllers/AddNumbersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTraining.Controllers
+{
+    public class AddNumbersController : Controller
+    {
+        // GET: AddNumbers
+        public ActionResult AddNumbers()
+        {
+            return View();
+        }
+    }
+}
