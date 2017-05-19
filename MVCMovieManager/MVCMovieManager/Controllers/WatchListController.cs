018fb7cdb2db5af18683bb1d0ab012a9816da97d
Ana Maria Ciuhan <ana.ciuhan@fortech.ro>
2017-05-19 18:01:30 +0300

    Signed-off-by: Ana Maria Ciuhan <ana.ciuhan@fortech.ro>

---
+++ MVCMovieManager/MVCMovieManager/Controllers/WatchListController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCMovieManager.Models;
+
+namespace MVCMovieManager.Controllers
+{
+    public class WatchListController : Controller
+    {
+        private MovieManagerEntities db = new MovieManagerEntities();
+
+        // GET: WatchList
+        public ActionResult Index()
+        {
+            var watchLists = db.WatchLists.Include(w => w.Movie);
+            return View(watchLists.ToList());
+        }
+
+        // GET: WatchList/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WatchList watchList = db.WatchLists.Find(id);
+            if (watchList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(watchList);
+        }
+
+        // GET: WatchList/Create
+        public ActionResult Create()
+        {
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title");
+            return View();
+        }
+
+        // POST: WatchList/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WatchListId,MovieId")] WatchList watchList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WatchLists.Add(watchList);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title", watchList.MovieId);
+            return View(watchList);
+        }
+
+        // GET: WatchList/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WatchList watchList = db.WatchLists.Find(id);
+            if (watchList == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title", watchList.MovieId);
+            return View(watchList);
+        }
+
+        // POST: WatchList/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WatchListId,MovieId")] WatchList watchList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(watchList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title", watchList.MovieId);
+            return View(watchList);
+        }
+
+        // GET: WatchList/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WatchList watchList = db.WatchLists.Find(id);
+            if (watchList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(watchList);
+        }
+
+        // POST: WatchList/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WatchList watchList = db.WatchLists.Find(id);
+            db.WatchLists.Remove(watchList);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
